08cf372ed09efdcbdce0ff02eee40995c583de65
samiul haque <mdsamiulhaque682@gmail.com>
2026-02-11 16:29:20 +0600

    add mail funtions.

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -111,6 +111,13 @@ cloudinary.config(
     secure=True
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 # Media storage setting
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
