147352ca8e555861ab85b7104170c6c5374f763c
samiul haque <mdsamiulhaque682@gmail.com>
2026-02-11 16:11:21 +0600

    Frontedn url added

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -186,6 +186,9 @@ SIMPLE_JWT = {
 
 
 DJOSER = {
+    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_SITE_NAME':'phimart',
     'SERIALIZERS': {
         'user_create': 'users.serializers.UserCreateSerializer',
         'current_user': 'users.serializers.UserSerializer',
